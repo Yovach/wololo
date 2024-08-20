3a1fe9ad57b8615eae470335c20b5e996814b3a3
Quentin <contact@yovach.dev>
2024-08-20 21:10:16 +0200

    feat(client): show error

--- client/src/main.ts
+++ client/src/main.ts
@@ -1,48 +1,56 @@
-import './css/style.css';
+import "./css/style.css";
 
-import { getAvailableFormats } from './helpers/api';
-import { isFormElement, isSelectElement } from './helpers/main';
+import { getAvailableFormats } from "./helpers/api";
+import { isFormElement, isSelectElement } from "./helpers/main";
 
 const formEl = document.getElementById("form");
 if (!isFormElement(formEl)) {
-    throw new Error("Missing or invalid form element")
+  throw new Error("Missing or invalid form element");
 }
 
 const selectEl = document.getElementById("select-formats");
 if (!isSelectElement(selectEl)) {
-    throw new Error("Missing or invalid format selector")
+  throw new Error("Missing or invalid format selector");
 }
 
 getAvailableFormats().then((availableFormats) => {
-    for (const format of availableFormats) {
-        const option = document.createElement("option");
-        option.textContent = format;
-        selectEl.appendChild(option);
-    }
-})
+  for (const format of availableFormats) {
+    const option = document.createElement("option");
+    option.textContent = format;
+    selectEl.appendChild(option);
+  }
+});
 
 formEl.addEventListener("submit", (evt: SubmitEvent) => {
-    evt.preventDefault();
+  evt.preventDefault();
 
-    const target = evt.currentTarget;
-    if (!isFormElement(target)) {
-        return;
-    }
+  const target = evt.currentTarget;
+  if (!isFormElement(target)) {
+    return;
+  }
 
-    import("./helpers/send-convert-file-request").then(({ sendConvertFileRequest }) => {
-        const formData = new FormData(target);
-        return sendConvertFileRequest(formData)
-    }).then(async (response) => {
+  import("./helpers/send-convert-file-request")
+    .then(({ sendConvertFileRequest }) => {
+      const formData = new FormData(target);
+      return sendConvertFileRequest(formData);
+    })
+    .then(async (response) => {
+      if (response.ok) {
         const blob = await response.blob();
         const filename = response.headers.get("x-file-name");
 
-        console.log(response);
         const tmpUrl = URL.createObjectURL(blob);
         const anchor = document.createElement("a");
         anchor.href = tmpUrl;
         anchor.download = filename ?? crypto.randomUUID();
         anchor.click();
-
         URL.revokeObjectURL(tmpUrl);
-    })
-})
+      } else {
+        const json = await response.json();
+        const errorMessageEl = document.getElementById("error-message");
+        if (errorMessageEl) {
+          errorMessageEl.textContent = json.error;
+        }
+      }
+    });
+});
