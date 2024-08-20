6a90774fc293d46b28afc7c7309632ac822ec7c8
Quentin <contact@yovach.dev>
2024-08-20 14:01:25 +0200

    feat: send convert file request

--- client/src/helpers/main.ts
+++ client/src/helpers/main.ts
@@ -1,3 +1,7 @@
 export function isSelectElement(htmlElement: HTMLElement | null): htmlElement is HTMLSelectElement {
     return htmlElement instanceof HTMLSelectElement;
 }
+
+export function isFormElement(htmlElement: HTMLElement | EventTarget | null): htmlElement is HTMLFormElement {
+    return htmlElement instanceof HTMLFormElement;
+}
