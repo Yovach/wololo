0797765e9df02aa31d8dc7ea4f71222405b13576
Quentin <contact@yovach.dev>
2024-08-25 18:36:56 +0200

    chore: get FRONT_ENV from env vars

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,8 +1,11 @@
+use std::env;
+
 use axum::{
     extract::DefaultBodyLimit,
     routing::{get, post},
     Router,
 };
+use dotenvy::dotenv;
 use http::{HeaderName, HeaderValue, Method};
 use tower_http::cors::CorsLayer;
 use tracing::info;
@@ -15,13 +18,25 @@ pub mod tmp_file;
 async fn main() {
     tracing_subscriber::fmt::init();
 
+    if let Ok(_) = dotenv() {
+        tracing::info!("environment file found");
+    }
+
+    let front_url = match env::var("FRONT_URL") {
+        Ok(value) => value,
+        Err(_) => {
+            tracing::error!("`FRONT_URL` is not set in environment");
+            return;
+        },
+    };
+
     let app = Router::new()
         .route("/convert-file", post(routes::accept_form))
         .route("/available-formats", get(routes::available_formats))
         .layer(DefaultBodyLimit::max(1024 * 1024 * 1024)) // 1 GB
         .layer(
             CorsLayer::new()
-                .allow_origin("http://localhost:5173".parse::<HeaderValue>().unwrap())
+                .allow_origin(front_url.parse::<HeaderValue>().unwrap())
                 .allow_methods([Method::GET])
                 .expose_headers([HeaderName::from_static("x-file-name")]),
         );
