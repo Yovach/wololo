6b9fff9e21e1448d113d16ec31ac13e036bcc170
Quentin <contact@yovach.dev>
2024-09-08 20:44:04 +0200

    feat: working on a way to convert images

--- server/src/formats.rs
+++ server/src/formats.rs
@@ -1,3 +1,12 @@
-/// List of supported output file extensions
+/// List of supported output file extensions for videos
 pub static SUPPORTED_VIDEO_FORMATS: &'static [&'static str] =
     &["webm", "wmv", "mkv", "mp4", "gif", "mp3", "ogg", "wav"];
+
+/// List of supported output file extensions for images
+pub static SUPPORTED_IMAGE_FORMATS: &'static [&'static str] =
+    &["png", "jpeg", "avif", "webp", "ico"];
+
+pub enum OutputFormat {
+    VIDEO,
+    IMAGE,
+}
---
+++ server/src/utils.rs
@@ -0,0 +1,49 @@
+use std::process::Command;
+
+use crate::{
+    errors::ConvertError,
+    formats::{OutputFormat, SUPPORTED_IMAGE_FORMATS, SUPPORTED_VIDEO_FORMATS},
+};
+
+fn detect_file_type(file_path: &str) -> Result<OutputFormat, ()> {
+    let is_image = SUPPORTED_IMAGE_FORMATS
+        .iter()
+        .any(|ext| file_path.ends_with(ext.to_owned()));
+    if is_image {
+        return Ok(OutputFormat::IMAGE);
+    }
+
+    let is_video = SUPPORTED_VIDEO_FORMATS
+        .iter()
+        .any(|ext| file_path.ends_with(ext.to_owned()));
+    if is_video {
+        return Ok(OutputFormat::VIDEO);
+    }
+
+    return Err(());
+}
+
+pub fn convert_file(file_path: &str, output_path: &str) -> Result<(), ConvertError> {
+    if let Ok(output_format) = detect_file_type(output_path) {
+        match output_format {
+            OutputFormat::IMAGE => {}
+            OutputFormat::VIDEO => {
+                if let Err(err) = Command::new("ffmpeg")
+                    .arg("-loglevel")
+                    .arg("quiet")
+                    .arg("-i")
+                    .arg(file_path)
+                    .arg(output_path)
+                    .spawn()
+                    .expect("I expected a result here")
+                    .wait_with_output()
+                {
+                    tracing::error!("an error occured while converting file (err: {})", err);
+                    return Err(ConvertError::DuringConversion);
+                }
+            }
+        }
+    }
+
+    return Ok(());
+}
