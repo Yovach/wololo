0985238fe1f39e7a1b37a0463e0596e195cdb4ba
Quentin <contact@yovach.dev>
2024-09-08 19:02:43 +0200

    ref: rename SUPPORTED_FORMATS to SUPPORTED_VIDEO_FORMATS

--- server/src/formats.rs
+++ server/src/formats.rs
@@ -1,3 +1,3 @@
 /// List of supported output file extensions
-pub static SUPPORTED_FORMATS: &'static [&'static str] =
+pub static SUPPORTED_VIDEO_FORMATS: &'static [&'static str] =
     &["webm", "wmv", "mkv", "mp4", "gif", "mp3", "ogg", "wav"];
