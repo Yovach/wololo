108c54ddbfc860ac8f8a03969c027ec2d42b6116
Quentin <contact@yovach.dev>
2024-10-26 16:51:42 +0200

    fix: remove "ico" as not compatible with libvips

--- server/src/formats.rs
+++ server/src/formats.rs
@@ -6,14 +6,16 @@ pub static SUPPORTED_AUDIO_FORMATS: &'static [&'static str] = &["mp3", "ogg", "w
 
 /// List of supported output file extensions for images
 pub static SUPPORTED_IMAGE_FORMATS: &'static [&'static str] =
-    &["png", "jpeg", "avif", "webp", "ico"];
+    &["png", "jpeg", "avif", "webp"];
 
 pub enum OutputFormat {
     VIDEO,
     IMAGE,
+    AUDIO,
 }
 
 pub fn is_output_format_supported(output: &str) -> bool {
     return self::SUPPORTED_VIDEO_FORMATS.contains(&output)
-        || self::SUPPORTED_IMAGE_FORMATS.contains(&output);
+        || self::SUPPORTED_IMAGE_FORMATS.contains(&output)
+        || self::SUPPORTED_AUDIO_FORMATS.contains(&output);
 }
