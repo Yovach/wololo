6edef48b13a4575e196ce6315c0f4522ee74175a
Quentin <contact@yovach.dev>
2025-03-20 12:47:45 +0000

    feat: rewrite convert file form to React

--- client/src/components/form/convert-file-form.tsx
+++ client/src/components/form/convert-file-form.tsx
@@ -1,7 +1,42 @@
+import { FormEvent, useCallback, useState } from "react";
+import { sendConvertFileRequest } from "../../helpers/send-convert-file-request";
+
 export function ConvertFileForm() {
+  const [errorMessage, setErrorMessage] = useState<string>();
+  const onSubmit = useCallback(async (evt: FormEvent<HTMLFormElement>) => {
+    evt.preventDefault();
+
+    const formData = new FormData(evt.currentTarget);
+    try {
+      const response = await sendConvertFileRequest(formData);
+      if (response.ok) {
+        const blob = await response.blob();
+        const filename = response.headers.get("x-file-name");
+
+        const tmpUrl = URL.createObjectURL(blob);
+        const anchor = document.createElement("a");
+        anchor.href = tmpUrl;
+        anchor.download = filename ?? crypto.randomUUID();
+        anchor.click();
+        URL.revokeObjectURL(tmpUrl);
+      } else {
+        const json = await response.json();
+        setErrorMessage(json.error);
+      }
+    } catch (e) {
+      setErrorMessage("An error occured");
+    }
+  }, []);
+
   return (
     <>
-      <form id="form" action="/" method="post" encType="multipart/form-data">
+      <form
+        id="form"
+        action="/"
+        method="post"
+        encType="multipart/form-data"
+        onSubmit={onSubmit}
+      >
         <div className="fields">
           <fieldset className="field-format">
             <label htmlFor="select-formats">
@@ -18,9 +53,11 @@ export function ConvertFileForm() {
         <button type="submit">Convert your file</button>
       </form>
 
-      <div className="error-container">
-        <span id="error-message"></span>
-      </div>
+      {errorMessage != null && (
+        <div className="error-container">
+          <span id="error-message">{errorMessage}</span>
+        </div>
+      )}
     </>
   );
 }
