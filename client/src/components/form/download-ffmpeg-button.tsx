b1b8b34bea899f209c83ddbbaae039f836dd854b
Quentin <contact@yovach.dev>
2025-03-24 19:49:20 +0100

    feat: download ffmpeg worker and update render if loaded

--- client/src/components/form/download-ffmpeg-button.tsx
+++ client/src/components/form/download-ffmpeg-button.tsx
@@ -1,22 +1,32 @@
 "use client";
 
 import { FFmpeg } from "@ffmpeg/ffmpeg";
-import { memo, useCallback, useRef } from "react";
+import { memo, useCallback, useRef, useState } from "react";
 
+import ffmpegCore from "@ffmpeg/core?url";
 import ffmpegWasm from "@ffmpeg/core/wasm?url";
+import ffmpegWorker from "@ffmpeg/ffmpeg/worker?url";
 
 export const DownloadFFmpegButton = memo(function DownloadFFmpegButton() {
   const ffmpegRef = useRef(new FFmpeg());
+  const [isLoaded, setIsLoaded] = useState(false);
 
   const downloadFFmpeg = useCallback(async () => {
     await ffmpegRef.current.load({
       wasmURL: ffmpegWasm,
+      coreURL: ffmpegCore,
+      workerURL: ffmpegWorker,
     });
+    setIsLoaded(true);
   }, []);
 
-  return (
+  return isLoaded === true ? (
     <button type="button" onClick={downloadFFmpeg}>
-      Download ffmpeg
+     convert
     </button>
+  ) : (
+    <button type="button" onClick={downloadFFmpeg}>
+    Download ffmpeg
+  </button>
   );
 });
