3e8fca1d8e404b1a60b44aa09d79518485c6ad55
Quentin <contact@yovach.dev>
2025-03-24 21:41:06 +0100

    feat: add convert feature locally

--- client/src/components/form/convert-file-form.tsx
+++ client/src/components/form/convert-file-form.tsx
@@ -3,6 +3,7 @@
 import { FormEvent, memo, use, useCallback, useMemo, useState } from "react";
 import { sendConvertFileRequest } from "../../helpers/send-convert-file-request";
 import { getAvailableFormats } from "../../helpers/api";
+import { useFFmpeg } from "../../hooks/use-ffmpeg";
 
 type Props = {
   availableFormatsPromise: ReturnType<typeof getAvailableFormats>;
@@ -17,31 +18,69 @@ const Translations = {
 export const ConvertFileForm = memo(function ConvertFileForm({
   availableFormatsPromise,
 }: Props) {
+  const { ffmpeg, isLoaded } = useFFmpeg();
   const [errorMessage, setErrorMessage] = useState<string>();
-  const onSubmit = useCallback(async (evt: FormEvent<HTMLFormElement>) => {
-    evt.preventDefault();
+  const onSubmit = useCallback(
+    async (evt: FormEvent<HTMLFormElement>) => {
+      evt.preventDefault();
 
-    const formData = new FormData(evt.currentTarget);
-    try {
-      const response = await sendConvertFileRequest(formData);
-      if (response.ok) {
-        const blob = await response.blob();
-        const filename = response.headers.get("x-file-name");
+      const formData = new FormData(evt.currentTarget);
 
+      let blob: Blob | null = null;
+      let fileName: string | null = null;
+
+      try {
+        if (isLoaded) {
+          const file = formData.get("file");
+          if (file instanceof File) {
+            const fileBytes = await file.arrayBuffer();
+            const fileContent = new Uint8Array(fileBytes);
+            await ffmpeg.writeFile(file.name, fileContent);
+
+            fileName = "output.mp4";
+
+            await ffmpeg.exec(["-i", file.name, fileName]);
+
+            const data = await ffmpeg.readFile(fileName);
+
+            blob = new Blob([data], { type: "video/mp4" });
+
+            const link = document.createElement("a");
+            link.href = URL.createObjectURL(blob);
+            link.download = fileName;
+
+            link.click();
+
+            URL.revokeObjectURL(link.href);
+            link.remove();
+
+            ffmpeg.deleteFile("output.mp4");
+          }
+        } else {
+          const response = await sendConvertFileRequest(formData);
+          if (response.ok) {
+            blob = await response.blob();
+            fileName = response.headers.get("x-file-name");
+          } else {
+            const json = await response.json();
+            setErrorMessage(json.error);
+          }
+        }
+      } catch (e) {
+        setErrorMessage("An error occured");
+      }
+
+      if (blob && fileName) {
         const tmpUrl = URL.createObjectURL(blob);
         const anchor = document.createElement("a");
         anchor.href = tmpUrl;
-        anchor.download = filename ?? crypto.randomUUID();
+        anchor.download = fileName ?? crypto.randomUUID();
         anchor.click();
         URL.revokeObjectURL(tmpUrl);
-      } else {
-        const json = await response.json();
-        setErrorMessage(json.error);
       }
-    } catch (e) {
-      setErrorMessage("An error occured");
-    }
-  }, []);
+    },
+    [isLoaded, ffmpeg]
+  );
 
   const formats = use(availableFormatsPromise).formats;
   const groups = useMemo(
