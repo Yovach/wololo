0d157a1cbafa191864aa2023c24f77a6b5cd574a
Quentin <contact@yovach.dev>
2025-03-24 12:45:32 +0000

    feat: handle unavailable server error

--- client/src/helpers/api.ts
+++ client/src/helpers/api.ts
@@ -1,27 +1,34 @@
-import { z } from "zod"
+import { z } from "zod";
 
 const responseSchema = z.object({
-    formats: z.object({
-        image: z.array(z.string()),
-        video: z.array(z.string()),
-        audio: z.array(z.string()),
-    })
+  formats: z.object({
+    image: z.array(z.string()),
+    video: z.array(z.string()),
+    audio: z.array(z.string()),
+  }),
 });
 
 type AvailableFormatsResponse = z.infer<typeof responseSchema>;
 
 export async function getAvailableFormats(): Promise<AvailableFormatsResponse> {
-    const request = await fetch(`${import.meta.env.VITE_BACK_URL}/available-formats`);
-    if (!request.ok) {
-        return {
-            formats: {
-                image: [],
-                video: [],
-                audio: [],
-            }
-        };
+  try {
+    const request = await fetch(
+      `${import.meta.env.VITE_BACK_URL}/available-formats`
+    );
+    
+    if (request.ok) {
+      const result = await request.json();
+      return responseSchema.parse(result);
     }
+  } catch (e) {
+    console.error(e);
+  }
 
-    const result = await request.json();
-    return responseSchema.parse(result);
+  return {
+    formats: {
+      image: [],
+      video: [],
+      audio: [],
+    },
+  };
 }
