371c6169ef4247b032633399025d4fafc464bc10
Quentin <contact@yovach.dev>
2025-03-27 13:03:42 +0000

    feat: add "Downloading" text while downloading ffmpeg

--- client/src/components/form/download-ffmpeg-button.tsx
+++ client/src/components/form/download-ffmpeg-button.tsx
@@ -2,8 +2,19 @@ import { memo } from "react";
 import { useFFmpeg } from "../../hooks/use-ffmpeg";
 
 export const DownloadFFmpegButton = memo(function DownloadFFmpegButton() {
-  const { download, isReady } = useFFmpeg();
-  return isReady === false && (
+  const { download, isReady, isLoading } = useFFmpeg();
+
+  if (isReady) {
+    return null;
+  }
+
+  if (isLoading) {
+    return (
+      <span>Downloading..</span>
+    );
+  }
+
+  return (
     <button type="button" onClick={download}>
       Download ffmpeg
     </button>
