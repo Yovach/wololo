e4ecf30aa3fa6dcdc4d53654355f35f247e9023b
Quentin <contact@yovach.dev>
2025-03-29 21:19:09 +0100

    ref: move modern-normalize css import to tsx

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,6 +1,7 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
+import "modern-normalize/modern-normalize.css";
 import "./css/style.css";
 
 createRoot(document.getElementById("app")!).render(
