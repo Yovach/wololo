b1f7c3f25a02c60915e485415e51b2f5203b0be9
Quentin <contact@yovach.dev>
2025-10-01 11:38:17 +0000

    chore: update TypeScript target to ES2022 and adjust Vite configuration

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -3,7 +3,10 @@ import preact from "@preact/preset-vite"
 
 export default defineConfig({
   plugins: [preact()],
+  build: {
+    target: "ES2022"
+  },
   optimizeDeps: {
-    exclude: ["@ffmpeg/ffmpeg"],
+    exclude: ["@ffmpeg/ffmpeg", "@ffmpeg/core"],
   },
 });
