061bb0df92440eb12f5da2819b08c650f9a5dfcc
Quentin <contact@yovach.dev>
2025-10-09 21:02:10 +0200

    fix: update DownloadFFmpegButton to call download without argument

--- client/src/components/form/download-ffmpeg-button.tsx
+++ client/src/components/form/download-ffmpeg-button.tsx
@@ -1,8 +1,9 @@
-import { memo } from "react";
+import { memo, useCallback } from "react";
 import { useFFmpeg } from "../../hooks/use-ffmpeg";
 
 export const DownloadFFmpegButton = memo(function DownloadFFmpegButton() {
   const { download, isReady, isLoading } = useFFmpeg();
+  const onClick = useCallback(() => download(), [download]);
 
   if (isReady) {
     return null;
@@ -13,7 +14,7 @@ export const DownloadFFmpegButton = memo(function DownloadFFmpegButton() {
   }
 
   return (
-    <button type="button" onClick={download}>
+    <button type="button" onClick={onClick}>
       Download ffmpeg
     </button>
   );
