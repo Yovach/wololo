930d6a1933ec52b52b5ffa8e6f4c19e0bdb2fd9e
Quentin <contact@yovach.dev>
2025-10-10 15:20:14 +0200

    fix(vite): re-enable minification in build configuration

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -11,7 +11,7 @@ export default defineConfig({
   ],
   build: {
     target: "ES2022",
-    minify: false,
+    minify: true,
   },
   optimizeDeps: {
     exclude: ["@ffmpeg/ffmpeg", "@ffmpeg/core"],
