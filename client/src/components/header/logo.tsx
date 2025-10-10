d0c36da2cd67716260df5a247f70c7b02d251c33
Quentin <contact@yovach.dev>
2025-10-10 16:17:33 +0200

    fix: format JSON and HTML files for consistency, fix tag closure in html

--- client/src/components/header/logo.tsx
+++ client/src/components/header/logo.tsx
@@ -1,6 +1,6 @@
 import { JSX, memo, NamedExoticComponent } from "react";
 
-const imgUrl = new URL('../../images/logo_wololo.png', import.meta.url).href
+const imgUrl = new URL("../../images/logo_wololo.png", import.meta.url).href;
 
 export const Logo: NamedExoticComponent = memo(function Logo(): JSX.Element {
   return <img src={imgUrl} />;
