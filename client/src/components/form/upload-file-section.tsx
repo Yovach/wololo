4e4c75788520f6189afc766b6fe39cef32599661
Quentin <contact@yovach.dev>
2025-10-16 18:44:47 +0200

    feat: move onDrop and onSelect callbacks in memoized functions

--- client/src/components/form/upload-file-section.tsx
+++ client/src/components/form/upload-file-section.tsx
@@ -2,6 +2,7 @@ import {
   ForwardRefExoticComponent,
   memo,
   RefObject,
+  useCallback,
   useRef,
   useState,
 } from "react";
@@ -17,20 +18,29 @@ import {
 import { useGetSupportedFormats } from "../../api/get-supported-formats";
 import { flatSupportedFormats } from "../../helpers/utils";
 import { UploadIcon } from "lucide-react";
+import { DropEvent } from "@react-types/shared";
 
 export const UploadFileSection = memo(function UploadFileSection() {
-  const [files, setFiles] = useState<FileDropItem[]>([]);
+  const [files, setFiles] = useState<File[]>([]);
+
+  const onDrop = useCallback(async (evt: DropEvent) => {
+    const fileDroppedItems: FileDropItem[] = evt.items.filter(
+      (file) => file.kind === "file",
+    );
+    const files = await Promise.all(
+      fileDroppedItems.map((file) => file.getFile()),
+    );
+    setFiles(files);
+  }, []);
+
+  const onSelect = useCallback((e: FileList | null) => {
+    setFiles(e ? Array.from(e) : []);
+  }, []);
+
   return (
     <section className="mt-16">
-      <DropZone
-        onDrop={(evt) => {
-          const files: FileDropItem[] = evt.items.filter(
-            (file) => file.kind === "file",
-          );
-          setFiles(files);
-        }}
-      >
-        <FileTrigger allowsMultiple onSelect={(e) => {}}>
+      <DropZone onDrop={onDrop}>
+        <FileTrigger allowsMultiple onSelect={onSelect}>
           <Button className="p-6 h-full bg-gray-200 rounded-xl cursor-pointer hover:scale-105 transition-all relative w-64 data-drop-target:border-amber-500 border-2 border-solid border-transparent flex justify-center items-center flex-col gap-y-2 shadow-lg hover:shadow-xl">
             <UploadIcon className="size-6" />
             <span className="text-base">Drop or click</span>
