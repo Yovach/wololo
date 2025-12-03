53bf5dc68e83e3837da01df565d59d13f4514473
Quentin <contact@yovach.dev>
2025-12-03 12:22:35 +0000

    feat: get all mime from mime-db package and filter by category

--- client/src/helpers/supported-formats.ts
+++ client/src/helpers/supported-formats.ts
@@ -1,21 +1,73 @@
+import mime from "mime-db";
+
+const filteredMimes = Object.entries(mime).filter(([key, value]) => {
+  const isTypeSupported =
+    key.startsWith("image") ||
+    key.startsWith("video") ||
+    key.startsWith("audio");
+  if (!isTypeSupported) {
+    return false;
+  }
+
+  const mimeChunk = key.split("/", 2)[1];
+  const isStandard =
+    mimeChunk != null &&
+    !mimeChunk.startsWith("vnd") &&
+    !mimeChunk.startsWith("x-");
+  if (!isStandard) {
+    return false;
+  }
+
+  return value.extensions != null && value.extensions.length > 0;
+});
+
+const videoFormats: string[] = filteredMimes
+  .filter(
+    ([key, value]) => key.startsWith("video") && value.extensions?.[0] != null,
+  )
+  .map(([, value]) => {
+    const firstExtension = value.extensions?.[0];
+    if (firstExtension == null) {
+      throw new Error("Can't be here");
+    }
+
+    return firstExtension;
+  });
+
 /// List of supported output file extensions for videos
-export const SUPPORTED_VIDEO_FORMATS: readonly string[] = Object.freeze([
-  "webm",
-  "mp4",
-  "gif",
-]);
+export const SUPPORTED_VIDEO_FORMATS: readonly string[] =
+  Object.freeze(videoFormats);
+
+const audioFormats: string[] = filteredMimes
+  .filter(
+    ([key, value]) => key.startsWith("audio") && value.extensions?.[0] != null,
+  )
+  .map(([, value]) => {
+    const firstExtension = value.extensions?.[0];
+    if (firstExtension == null) {
+      throw new Error("Can't be here");
+    }
+
+    return firstExtension;
+  });
 
 /// List of supported output file extensions for audios
-export const SUPPORTED_AUDIO_FORMATS: readonly string[] = Object.freeze([
-  "mp3",
-  "ogg",
-  "wav",
-]);
+export const SUPPORTED_AUDIO_FORMATS: readonly string[] =
+  Object.freeze(audioFormats);
+
+const imageFormats: string[] = filteredMimes
+  .filter(
+    ([key, value]) => key.startsWith("image") && value.extensions?.[0] != null,
+  )
+  .map(([, value]) => {
+    const firstExtension = value.extensions?.[0];
+    if (firstExtension == null) {
+      throw new Error("Can't be here");
+    }
+
+    return firstExtension;
+  });
 
 /// List of supported output file extensions for images
-export const SUPPORTED_IMAGE_FORMATS: readonly string[] = Object.freeze([
-  "png",
-  "jpeg",
-  "avif",
-  "webp",
-]);
+export const SUPPORTED_IMAGE_FORMATS: readonly string[] =
+  Object.freeze(imageFormats);
