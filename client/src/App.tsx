6f9456c9fd35785768d7b71a8daa435f616354f5
Quentin FLAMANT <quentin.flamant@pm.me>
2026-06-06 14:51:40 -0400

    chore(client): remove sonner dependency

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,17 +1,12 @@
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { JSX } from "react/jsx-runtime";
-import { Toaster } from "sonner";
-import { DownloadFFmpegSection } from "./components/form/download-ffmpeg-section";
 import { UploadFileSection } from "./components/form/upload-file-section";
-import { Logo } from "./components/header/logo";
-import { FFmpegProvider } from "./contexts/ffmpeg-context";
 
 const queryClient = new QueryClient();
 
 function App(): JSX.Element {
   return (
     <QueryClientProvider client={queryClient}>
-      <Toaster richColors closeButton />
         <div>
 
         {/* <Logo /> */}
