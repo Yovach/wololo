d592d2493d419ce7c75f90fcff61b0603c4ec393
Quentin FLAMANT <quentin.flamant@pm.me>
2026-06-06 14:41:30 -0400

    chore(client): add sonner toaster to app

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,5 +1,6 @@
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { JSX } from "react/jsx-runtime";
+import { Toaster } from "sonner";
 import { DownloadFFmpegSection } from "./components/form/download-ffmpeg-section";
 import { UploadFileSection } from "./components/form/upload-file-section";
 import { Logo } from "./components/header/logo";
@@ -10,6 +11,7 @@ const queryClient = new QueryClient();
 function App(): JSX.Element {
   return (
     <QueryClientProvider client={queryClient}>
+      <Toaster richColors closeButton />
         <div>
 
         {/* <Logo /> */}
